85d22a1a3518884ca09c43851f6cd3f3b30cc542
sabreennt <sabreenas.ih@gmail.com>
2026-04-14 22:19:08 +0700

    koneksi

--- koneksi.php
+++ koneksi.php
@@ -9,8 +9,10 @@ $koneksi = mysqli_connect($host, $user, $pass, $db);
 if (!$koneksi) {
     die("Koneksi ke database gagal: " . mysqli_connect_error());
 }
-?>
 
 /*
-* File: koneksi.php
-* Description: This file establishes a connection to the MySQL database using the mysqli extension.
+
+File: koneksi.php
+
+Description: This file establishes a connection to the MySQL database using the mysqli extension.
+*/
